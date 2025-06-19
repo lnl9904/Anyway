d721a1788d38a9e168a97a99bd99e99875c9acf2
lnl9904 <lenholoc132@gmail.com>
2025-06-19 17:43:02 +0700

    Add files via upload

---
+++ asm7/7.1.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int n, x;
+    printf("Nhap n, x: ");
+    scanf("%d %d", &n, &x);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        printf("arr[%d] = ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    int closeVal = arr[0], minDiff = abs(arr[0] - x);
+    for (int i = 1; i < n; i++) {
+        int diff = abs(arr[i] - x);
+        if (diff < minDiff) {
+            minDiff = diff;
+            closeVal = arr[i];
+        }
+    }
+
+    printf("Phan tu gan x nhat la: %d (|%d - %d| = %d)\n", closeVal, closeVal, x, minDiff);
+    
+}
+
---
+++ asm7/7.4.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+    int n;
+    printf("Nhap n: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        printf("arr[%d] = ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    int mn = arr[0], mx = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < mn) mn = arr[i];
+        if (arr[i] > mx) mx = arr[i];
+    }
+
+    printf("Doan chua tat ca phan tu: [%d, %d]\n", mn, mx);
+}
+
